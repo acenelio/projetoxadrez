74b82c2cf35c88ad239e2faecb32b97d1bbf43e2
Nelio Alves <nelio.cursos@gmail.com>
2017-11-22 10:34:39 -0200

    Projeto iniciado

---
+++ projetoxadrez/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace projetoxadrez {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá");
+            Console.ReadLine();
+        }
+    }
+}
